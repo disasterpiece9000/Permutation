e5b1f004ed0f911ca96fa7ad096585a02fdc7b36
HaydenCarroll <44235446+HaydenCarroll@users.noreply.github.com>
2019-12-03 21:37:23 -0500

    Add files via upload

--- htdocs/stats.js
+++ htdocs/stats.js
@@ -31,18 +31,24 @@ function setSelectedPlaylist()
         {
             table.removeChild(table.firstChild);
         }
-        for(var i=0;i<playlistData['tracks'].length;i++)
+        table.insertRow(0)
+        for(var i=1;i<playlistData['tracks'].length;i++)
         {
             var title = playlistData['tracks'][i]["title"];
             var artist = playlistData['tracks'][i]["artist"];
             var album = playlistData['tracks'][i]["album"];
             var date_added = playlistData['tracks'][i]["date added"];
             var listen_count = playlistData['tracks'][i]["listen count"];
-           
+            var img_src = playlistData['tracks'][i]['src'];
             var row = table.insertRow(i);
             row.setAttribute("class",'confrimTr');
             var cell0 = row.insertCell(0);
             var cell1 = row.insertCell(1);
+            var cell2 = row.insertCell(2);
+            var cell3 = row.insertCell(3);
+            var date = new Date(date_added*1000);
+            cell2.innerHTML = date.getMonth()+"/"+date.getDay()+"/"+date.getFullYear();
+            cell3.innerHTML = listen_count;
             cell0.setAttribute('class', 'confirmTd');
             cell1.setAttribute('class', 'confirmTd');
             cell1.setAttribute("style","user-select: none;");
@@ -51,7 +57,7 @@ function setSelectedPlaylist()
             var img = document.createElement('img');
             var altImg = document.createElement('img');
             altImg.src = "alt.png";
-            img.src=img_url;
+            img.src=img_src;
             img.setAttribute("onerror","this.src='alt.png';");
             cell0.appendChild(img);
             cell1.innerHTML=title+" - "+
